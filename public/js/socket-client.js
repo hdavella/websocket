c6e0472d77d02934b04aa35e1c246a05ffee0a3f
hdavella <hdavella@gmail.com>
2023-11-13 20:11:20 -0300

    Socket server side - console client side

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,11 @@
 console.log("Hola mundo");
+
+const clientSocket = io();
+
+clientSocket.on('connect', () => {
+    console.log("Connect - Client side");
+});
+
+clientSocket.on('disconnect', () => {
+    console.log("Disconnect - Client side");
+});
