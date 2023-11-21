c04a1c512a5cb73d7162c1cb8bc4e68db21dc1d3
hdavella <hdavella@gmail.com>
2023-11-21 17:43:22 -0300

    frontend

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,11 +1,22 @@
 console.log("Hola mundo");
 
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+const txtMessage = document.querySelector("#txtMessage");
+const btnSend = document.querySelector("#btnSend");
+
 const clientSocket = io();
 
 clientSocket.on('connect', () => {
     console.log("Connect - Client side");
+
+    lblOnline.style.display= '';
+    lblOffline.style.display='none';
 });
 
 clientSocket.on('disconnect', () => {
     console.log("Disconnect - Client side");
+
+    lblOnline.style.display= 'none';
+    lblOffline.style.display='';
 });
